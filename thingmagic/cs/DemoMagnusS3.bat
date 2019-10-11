65a723f9c61ed3085201ed79acf7ebfd371ca86a
Gabriel Seitz <gabe.seitz@axzon.com>
2019-10-11 12:21:29 -0500

    added support for network readers (Sargas)

--- thingmagic/cs/DemoMagnusS3.bat
+++ thingmagic/cs/DemoMagnusS3.bat
@@ -2,6 +2,7 @@ echo off
 if not exist ".\build" mkdir .\build
 copy lib\MercuryAPI.dll build\MercuryAPI.dll
 copy lib\ZeroconfService.dll build\ZeroconfService.dll
+copy lib\LLRP.dll build\LLRP.dll
 cls
 csc /lib:lib /r:MercuryAPI.dll,ZeroconfService.dll /out:build\MagnusS3.exe src\Common.cs src\MagnusS3.cs 
 if %ERRORLEVEL% EQU 0 (
--- thingmagic/cs/DemoXerxes.bat
+++ thingmagic/cs/DemoXerxes.bat
@@ -2,6 +2,7 @@ echo off
 if not exist ".\build" mkdir .\build
 copy lib\MercuryAPI.dll build\MercuryAPI.dll
 copy lib\ZeroconfService.dll build\ZeroconfService.dll
+copy lib\LLRP.dll build\LLRP.dll
 cls
 csc /lib:lib /r:MercuryAPI.dll,ZeroconfService.dll /out:build\Xerxes.exe src\Common.cs src\Xerxes.cs 
 if %ERRORLEVEL% EQU 0 (
