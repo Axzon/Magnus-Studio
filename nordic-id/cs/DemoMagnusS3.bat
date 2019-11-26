73b68f36e733237ff7e3735e0ac86b31317f273b
Gabriel Seitz <gabe.seitz@axzon.com>
2019-11-26 17:01:27 -0600

    removing absolute path to csc.exe

--- nordic-id/cs/DemoMagnusS3.bat
+++ nordic-id/cs/DemoMagnusS3.bat
@@ -2,7 +2,7 @@
 if not exist ".\build" mkdir .\build
 copy lib\NurApiDotNet.dll build\NurApiDotNet.dll
 copy lib\NURAPI.dll build\NURAPI.dll
-C:\Windows\Microsoft.NET\Framework\v3.5\csc /lib:lib /r:NurApiDotNet.dll /out:build\MagnusS3.exe src\Common.cs src\MagnusS3.cs 
+csc /lib:lib /r:NurApiDotNet.dll /out:build\MagnusS3.exe src\Common.cs src\MagnusS3.cs 
 if %ERRORLEVEL% EQU 0 (
     build\MagnusS3
 )
