ec15614cb5b64eda05f9ddef6aa61eb96a0138ef
Gabriel Seitz <gabe.seitz@axzon.com>
2019-11-26 16:18:26 -0600

    returning DemoMagnusS3.bat to default state

--- nordic-id/cs/DemoMagnusS3.bat
+++ nordic-id/cs/DemoMagnusS3.bat
@@ -1,10 +1,10 @@
 @echo off
+cls
 if not exist ".\build" mkdir .\build
 copy lib\NurApiDotNet.dll build\NurApiDotNet.dll
 copy lib\NURAPI.dll build\NURAPI.dll
-C:\Windows\Microsoft.NET\Framework\v2.0.50727\csc /lib:lib /r:NurApiDotNet.dll /out:build\MagnusS3.exe src\Common.cs src\MagnusS3.cs 
+csc /lib:lib /r:NurApiDotNet.dll /out:build\MagnusS3.exe src\Common.cs src\MagnusS3.cs 
 if %ERRORLEVEL% EQU 0 (
-    REM cls
     build\MagnusS3
 )
 pause
